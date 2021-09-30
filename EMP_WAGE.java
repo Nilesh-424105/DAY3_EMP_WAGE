507030b97d784d62d466591729721a1443cda1f4
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-09-30 15:32:17 +0530

    ADDEA: CALCULATE_DAILLY_EMPLOYEE_WAGE

--- EMP_WAGE.java
+++ EMP_WAGE.java
@@ -1,7 +1,27 @@
 
 public class EMP_WAGE {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+    // Constants
+    static final int IS_FULL_TIME = 1;
+    static final int EMP_RATE_PER_HOUR = 20;
+
+    public static void main(String[] args) {
+
+           // Initializing variables
+           int EmpWage = 0, empHrs = 0;
+
+           // Computations
+           double empCheck = Math.floor(Math.random() * 10) % 2;
+
+           if(empCheck == IS_FULL_TIME) {
+                 System.out.println("Employee is Present");
+                      empHrs = 8;
+           } else {
+                 System.out.println("Employee is Absent");
+                      empHrs = 0;
+           }
+           EmpWage = empHrs * EMP_RATE_PER_HOUR;
+           System.out.println("To Calculate Dailly Employee Wage: " + EmpWage);
+
 	}
 
 }
