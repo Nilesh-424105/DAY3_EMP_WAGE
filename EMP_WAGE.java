bc308f1841d5c6349c34ccd07f5ef6e535bbbb21
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-09-30 15:40:06 +0530

    ADDED: ADD_PART_TIME_EMPLOYEE_AND_WAGE

--- EMP_WAGE.java
+++ EMP_WAGE.java
@@ -1,7 +1,28 @@
 
 public class EMP_WAGE {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+    // Constants
+    static final int IS_PART_TIME = 1;
+    static final int IS_FULL_TIME = 2;
+    static final int EMP_RATE_PER_HOUR = 20;
+
+    public static void main(String[] args) {
+
+        // Initializing variables
+        int EmpWage = 0, empHrs = 0;
+
+        // Computations
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+
+        if(empCheck == IS_FULL_TIME) {
+                    empHrs = 8;
+        } else if(empCheck == IS_PART_TIME) {
+                    empHrs = 4;
+        } else {
+                    empHrs = 0;
+        }
+        EmpWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("To Calculate Dailly Employee Wage: "+EmpWage);
+
 	}
 
 }
